3073ee8bc14b1b1f7da18dbfb7f88bba3c609851
nxd258 <120349144+nxd258@users.noreply.github.com>
2025-12-04 17:58:02 +0700

    Update index.js
    embed

--- bot_discord/index.js
+++ bot_discord/index.js
@@ -31,7 +31,6 @@ const GAS_WEBHOOK_URL =
   "https://script.google.com/macros/s/AKfycbwPPRtBxzURgpw2WxStHEBRtt9E3TKM9S6vpAGlq1V8kSH6KY2z6c_DrKWoEKY36Mj4/exec";
 
 // Hàm cắt text dài thành từng đoạn nhỏ
-const MAX_EMBED_LENGTH = 3500; // an toàn hơn 4000
 export function splitMessagePreserveLinks(text: string, maxLength = 3500): string[] {
   if (!text) return [];
 
